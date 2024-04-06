1ba0a6323bb4151725bf9b42ef960cbe3e77dfd2
jaylenperez <jaylenestella24@gmail.com>
2024-04-06 13:26:49 -0500

    Created a empty  README and created header file

---
+++ monty.h
@@ -0,0 +1,17 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+typedef struct stack_s
+{
+    int n;
+    struct stack_s *prev;
+    struct stack_s *next;
+} stack_t;
+
+typedef struct instruction_s
+{
+    char *opcode;
+    void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+#endif
