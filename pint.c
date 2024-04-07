05f718ae6de68bdf8653548cb09ecacfd485e507
jaylenperez <jaylenestella24@gmail.com>
2024-04-07 18:10:37 -0500

    took out voiding of line_number

--- pint.c
+++ pint.c
@@ -9,8 +9,6 @@
 
 void pint(stack_t **stack, unsigned int line_number)
 {
-	(void)line_number;
-
 	if (stack == NULL || *stack == NULL)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
