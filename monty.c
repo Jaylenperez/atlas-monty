2a9779b463fb5a9ed2ccd164b946bffaa18cc2ec
jaylenperez <jaylenestella24@gmail.com>
2024-04-07 15:18:42 -0500

    initialization of variables for file handling and stack management, check for correct number of command-line args, loop to read lines from file, tokenize, and execute, and cleanup

--- monty.c
+++ monty.c
@@ -1,19 +1,49 @@
 #include "monty.h"
 
 /**
- * main - Entry point
- * @argc: The number of command-line arguments.
- * @argv: A pointer to an array of strings containing
- * the command-line arguments.
- * Description: print alphabets in lowercase using the putchar
- * 
- * Return: returns 0 (Success)
+ * main - Entry point for the Monty ByteCode interpreter
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
  */
-int main(int argc, char **argv)
+
+int main(int argc, char *argv[])
 {
-    (void) argv;
+    FILE *file;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    unsigned int line_number = 0;
+    char *opcode;
 
-    validate_arguments(argc);
+    stack_t *stack = NULL;
 
-    return (0);
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    file = fopen(argv[1], "r");
+    if (!file)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    while ((read = getline(&line, &len, file)) != -1)
+    {
+        line_number++;
+        opcode = strtok(line, " \n\t");
+
+        if (opcode && opcode[0] != '#')
+        {
+            execute(opcode, &stack, line_number);
+        }
+    }
+
+    fclose(file);
+    free(line);
+    free_node(&stack);
+    return (EXIT_SUCCESS);
 }
