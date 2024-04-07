6ab22262599118d232a15e2388dfdae25f4760cd
jaylenperez <jaylenestella24@gmail.com>
2024-04-07 17:45:01 -0500

    reverted

--- pint.c
+++ pint.c
@@ -9,7 +9,9 @@
 
 void pint(stack_t **stack, unsigned int line_number)
 {
-	if (*stack == NULL)
+	(void)line_number;
+
+	if (stack == NULL || *stack == NULL)
 	{
 		fprint(stderr, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
