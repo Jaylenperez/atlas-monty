c365baaee64fbb43bb1e1e049b0109f368bde312
jaylenperez <jaylenestella24@gmail.com>
2024-04-07 17:14:35 -0500

    convert indentation to tabs

--- monty.c
+++ monty.c
@@ -9,41 +9,41 @@
 
 int main(int argc, char *argv[])
 {
-    FILE *file;
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    unsigned int line_number = 0;
-    char *opcode;
+	FILE *file;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	unsigned int line_number = 0;
+	char *opcode;
 
-    stack_t *stack = NULL;
+	stack_t *stack = NULL;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
 
-    file = fopen(argv[1], "r");
-    if (!file)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
+	file = fopen(argv[1], "r");
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 
-    while ((read = getline(&line, &len, file)) != -1)
-    {
-        line_number++;
-        opcode = strtok(line, " \n\t");
+	while ((read = getline(&line, &len, file)) != -1)
+	{
+		line_number++;
+		opcode = strtok(line, " \n\t");
 
-        if (opcode && opcode[0] != '#')
-        {
-            execute(opcode, &stack, line_number);
-        }
-    }
+		if (opcode && opcode[0] != '#')
+		{
+			execute(opcode, &stack, line_number);
+		}
+	}
 
-    fclose(file);
-    free(line);
-    free_node(&stack);
-    return (EXIT_SUCCESS);
+	fclose(file);
+	free(line);
+	free_node(&stack);
+	return (EXIT_SUCCESS);
 }
