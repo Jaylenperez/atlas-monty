66771438ca122755e559a2c109f4e252cbf4098b
jaylenperez <jaylenestella24@gmail.com>
2024-04-07 21:33:18 -0500

    implemented the nop op code

--- execute.c
+++ execute.c
@@ -18,6 +18,7 @@ void execute(char *opcode, stack_t **stack, unsigned int line_number)
 			{"pop", pop},
 			{"swap", swap},
 			{"add", add},
+			{"nop", nop},
 			{NULL, NULL}};
 
 	for (i = 0; instructions[i].opcode; i++)
