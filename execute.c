e964b7a2adeebc62d1cfe20ffc3b821bc9653c82
jaylenperez <jaylenestella24@gmail.com>
2024-04-07 21:04:47 -0500

    implement swap opcode

--- execute.c
+++ execute.c
@@ -16,6 +16,7 @@ void execute(char *opcode, stack_t **stack, unsigned int line_number)
 			{"pall", pall},
 			{"pint", pint},
 			{"pop", pop},
+			{"swap", swap},
 			{NULL, NULL}};
 
 	for (i = 0; instructions[i].opcode; i++)
